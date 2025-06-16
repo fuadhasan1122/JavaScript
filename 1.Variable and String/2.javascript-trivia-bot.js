a2ddbd3d5a12b1cbf8200ee3ddcd082929e5a3e3
fuadhasan1122 <fuad43002@gmail.com>
2025-06-16 15:03:47 +0600

    1.variable and string 2. Booleans and Numbers

---
+++ 1.Variable and String/2.javascript-trivia-bot.js
@@ -0,0 +1,20 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Fuad";
+let botLocation = "Sylhet";
+let favoriteLanguage = "JS";
+
+console.log("My name is " +botName+ " and I live on " +botLocation+".");
+
+console.log("My favorite programming language is "+ favoriteLanguage +".");
+
+let codingFact = favoriteLanguage+ " is Fun.";
+console.log(codingFact);
+
+ codingFact = favoriteLanguage+ " is more Fun.";
+console.log(codingFact);
+
+ codingFact = favoriteLanguage+ " is  more more Fun.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+ " from "+botLocation+".");
---
+++ 1.Variable and String/3.Build_a_sentence_maker.js
@@ -0,0 +1,25 @@
+let adjective = "good";
+let noun = "Fuad";
+let verb = "work";
+let place  = "sylhet";
+let adjective2 = "very well";
+let noun2  = "Nabik";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + 
+". The " + noun + " lived in a " + place + " and had " + adjective2 + 
+" nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: "+firstStory);
+
+ adjective = "fine";
+  noun = "jond";
+ verb = "eat";
+ place  = "dhaka";
+ adjective2 = "well";
+ noun2  = "dong";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + 
+". The " + noun + " lived in a " + place + " and had " + adjective2 + 
+" nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: "+secondStory);
